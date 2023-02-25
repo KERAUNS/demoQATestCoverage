b8a53b994d385ef36bafa0f41bd19834367594eb
kerauns <vesstar.dj@gmail.com>
2023-02-25 22:30:00 +0300

    added test coverage, added empty void validator

--- src/test/java/DemoQAForm.java
+++ src/test/java/DemoQAForm.java
@@ -1,31 +1,56 @@
-import static com.codeborne.selenide.Selenide.$;
-
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import static com.codeborne.selenide.conditions.Text.text;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class DemoQAForm {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1000x700";
         Configuration.baseUrl = "https://demoqa.com";
-    }
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserPosition = "0x0";
+                    }
 
     @Test
     void fillFormTest() {
-        open("/text-box");
+        File jpg = new File("src/test/resources/neon_lights.jpg");
+        open("/automation-practice-form");
+        executeJavaScript("const footer = document.querySelector('footer'); footer.setAttribute('hidden', '');");
+        $("#firstName").setValue("Vasiliy");
+        $("#lastName").setValue("Starnov");
+        $("#userEmail").setValue("vasiliy.starnov@mail.com");
+        $("label[for=gender-radio-1]").click();
+        $("input#userNumber").clear();
+        $("input#userNumber").setValue("88005553535");
+        $("#dateOfBirthInput").setValue("12.12.1991").pressEnter();
+        $("input#subjectsInput").setValue("Compute").pressEnter();
+        $("label[for=hobbies-checkbox-2]").click();
+        $("input#uploadPicture").uploadFile(jpg);
+        $("textarea#currentAddress").setValue("sdfsdfsdf");
+        $("div#state").click();
+        $(new ByText("NCR")).click();
+        $("div#city").click();
+        $(new ByText("Noida")).click();
+        $("div#city").click();
+        $("button#submit").pressEnter();
+    }
+    @Test
+    void validateFormFill() {
+        $("table.table").shouldHave(text("Vasiliy Starnov"));
+        $("table.table").shouldHave(text("vasiliy.starnov@mail.com"));
+        $("table.table").shouldHave(text("Male"));
+        $("table.table").shouldHave(text("8800555353"));
+        $("table.table").shouldHave(text("25 February,202312"));
+        $("table.table").shouldHave(text("Computer Science"));
+        $("table.table").shouldHave(text("Reading"));
+        $("table.table").shouldHave(text("neon_lights.jpg"));
+        $("table.table").shouldHave(text("sdfsdfsdf"));
+        $("table.table").shouldHave(text("NCR Noida"));
 
-        $("#userName").setValue("Alex Egorov");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some address 1");
-        $("#permanentAddress").setValue("Another address 1");
-        $("#submit").click();
-
-        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
-                text("Some address 1"), text("Another address 1"));
     }
 }
